00469289863013bbb2f0b532497542cc9c4bb8a5
Rima Falko <fembina@pm.me>
2024-05-11 07:39:43 +0200

    Added custom telegram bridge and refactored telegram to unibot processing pipeline

---
+++ Sources/Falko.Unibot.Bridges.Telegram/Models/Update.cs
@@ -0,0 +1,25 @@
+namespace Falko.Unibot.Bridges.Telegram.Models;
+
+public sealed class Update(
+    long updateId,
+    Message? message = null,
+    Message? editedMessage = null,
+    Message? channelPost = null,
+    Message? editedChannelPost = null,
+    Message? businessMessage = null,
+    Message? editedBusinessMessage = null)
+{
+    public readonly long UpdateId = updateId;
+
+    public readonly Message? Message = message;
+
+    public readonly Message? EditedMessage = editedMessage;
+
+    public readonly Message? ChannelPost = channelPost;
+
+    public readonly Message? EditedChannelPost = editedChannelPost;
+
+    public readonly Message? BusinessMessage = businessMessage;
+
+    public readonly Message? EditedBusinessMessage = editedBusinessMessage;
+}
