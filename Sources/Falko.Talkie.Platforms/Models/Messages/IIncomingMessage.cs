1afbd38ac710d61934e379f38c28e59db87eb6fd
Rimu Falko <fembina@pm.me>
2024-06-04 17:58:06 +0200

    Added incoming message reply

--- Sources/Falko.Talkie.Platforms/Models/Messages/IIncomingMessage.cs
+++ Sources/Falko.Talkie.Platforms/Models/Messages/IIncomingMessage.cs
@@ -1,6 +1,6 @@
 namespace Talkie.Models.Messages;
 
-public interface IIncomingMessage : Message.IWithPlatform, Message.IWithIdentifier, Message.IWithEntry
+public interface IIncomingMessage : Message.IWithPlatform, Message.IWithIdentifier, Message.IWithEntry, Message.IWithReply
 {
     IIncomingMessage Mutate(Func<IIncomingMessageMutator, IIncomingMessageMutator> mutation);
 }
