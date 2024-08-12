3e616dbfc4a9a9945dedd1c005cb523da793e19b
Rima Falko <fembina@pm.me>
2024-08-12 21:19:13 +0200

    Added publish silently features

--- Sources/Falko.Talkie.Bridges.Telegram/Models/SendMessage.cs
+++ Sources/Falko.Talkie.Bridges.Telegram/Models/SendMessage.cs
@@ -3,11 +3,14 @@ namespace Talkie.Bridges.Telegram.Models;
 public sealed class SendMessage(
     long chatId,
     string text,
+    bool? disableNotification = null,
     ReplyParameters? replyParameters = null)
 {
     public readonly long ChatId = chatId;
 
     public readonly string Text = text;
+
+    public readonly bool? DisableNotification = disableNotification;
 
     public readonly ReplyParameters? ReplyParameters = replyParameters;
 }
