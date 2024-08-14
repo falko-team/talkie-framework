d84ab7a60ab2c5870fca64ac9aeafc013b2488b4
Rima Falko <rima@falko.email>
2024-08-14 18:38:50 +0200

    Added mixins and unobserved mixin and TakeUnobservedExceptionAsync extension

---
+++ Sources/Falko.Talkie.Core/Mixins/IWith.cs
@@ -0,0 +1,7 @@
+namespace Talkie.Mixins;
+
+/// <summary>
+/// Represents a mixin for a target type
+/// </summary>
+/// <typeparam name="T">The type of the mixin target type.</typeparam>
+public interface IWith<T>;
--- Sources/Falko.Talkie.Signals/Signals/Signal.cs
+++ Sources/Falko.Talkie.Signals/Signals/Signal.cs
@@ -1,3 +1,18 @@
+using Talkie.Flows;
+
 namespace Talkie.Signals;
 
+/// <summary>
+/// Represents a <see cref="Signal"/> that can publish on a <see cref="ISignalFlow"/>.
+/// </summary>
+/// <remarks>
+/// A <see cref="Signal"/> is should be immutable
+/// and be used to represent a context
+/// that can be published on a <see cref="ISignalFlow"/>.
+/// </remarks>
+/// <example lang="csharp">
+/// var flow = new SignalFlow();
+/// var signal = new MySignal();
+/// await flow.PublishAsync(signal);
+/// </example>
 public abstract record Signal;
