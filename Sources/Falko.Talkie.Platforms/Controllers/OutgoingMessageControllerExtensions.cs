295b2e3345b2a646f41971a5887855e0752b0eec
Rima Falko <rima@falko.email>
2024-08-14 18:37:12 +0200

    Added without features extensions for PublishMessageAsync

--- Sources/Falko.Talkie.Platforms/Controllers/OutgoingMessageControllerExtensions.cs
+++ Sources/Falko.Talkie.Platforms/Controllers/OutgoingMessageControllerExtensions.cs
@@ -11,11 +11,23 @@ public static partial class OutgoingMessageControllerExtensions
         return controller.PublishMessageAsync(mutator.Mutate(), features, cancellationToken);
     }
 
+    public static Task PublishMessageAsync(this IOutgoingMessageController controller, IOutgoingMessageMutator mutator,
+        CancellationToken cancellationToken)
+    {
+        return controller.PublishMessageAsync(mutator, default, cancellationToken);
+    }
+
     public static Task PublishMessageAsync(this IOutgoingMessageController controller, IOutgoingMessageBuilder builder,
         MessagePublishingFeatures features = default,
         CancellationToken cancellationToken = default)
     {
         return controller.PublishMessageAsync(builder.Build(), features, cancellationToken);
+    }
+
+    public static Task PublishMessageAsync(this IOutgoingMessageController controller, IOutgoingMessageBuilder builder,
+        CancellationToken cancellationToken)
+    {
+        return controller.PublishMessageAsync(builder, default, cancellationToken);
     }
 
     public static Task PublishMessageAsync(this IOutgoingMessageController controller, Func<IOutgoingMessageBuilder, IOutgoingMessageBuilder> builderFactory,
@@ -25,10 +37,28 @@ public static partial class OutgoingMessageControllerExtensions
         return controller.PublishMessageAsync(builderFactory(new OutgoingMessageBuilder()).Build(), features, cancellationToken);
     }
 
+    public static Task PublishMessageAsync(this IOutgoingMessageController controller, Func<IOutgoingMessageBuilder, IOutgoingMessageBuilder> builderFactory,
+        CancellationToken cancellationToken)
+    {
+        return controller.PublishMessageAsync(builderFactory, default, cancellationToken);
+    }
+
     public static Task PublishMessageAsync(this IOutgoingMessageController controller, string text,
         MessagePublishingFeatures features = default,
         CancellationToken cancellationToken = default)
     {
         return controller.PublishMessageAsync(message => message.AddText(text), features, cancellationToken);
+    }
+
+    public static Task PublishMessageAsync(this IOutgoingMessageController controller, string text,
+        CancellationToken cancellationToken)
+    {
+        return controller.PublishMessageAsync(text, default, cancellationToken);
+    }
+
+    public static Task PublishMessageAsync(this IOutgoingMessageController controller, IOutgoingMessage message,
+        CancellationToken cancellationToken)
+    {
+        return controller.PublishMessageAsync(message, default, cancellationToken);
     }
 }
