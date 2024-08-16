8ba951ea66f1cdc8045896d41f0f56411c9cfed6
Rima Falko <fembina@pm.me>
2024-08-16 06:12:51 +0200

    Added more modern connections (#102)

--- Sources/Falko.Talkie.Signals.Connections/Connectors/ISignalConnector.cs
+++ Sources/Falko.Talkie.Signals.Connections/Connectors/ISignalConnector.cs
@@ -5,5 +5,10 @@ namespace Talkie.Connectors;
 
 public interface ISignalConnector
 {
+    /// <summary>
+    /// Connects the connector to a <see cref="ISignalFlow"/>.
+    /// </summary>
+    /// <param name="flow">The <see cref="ISignalFlow"/> to connect to.</param>
+    /// <returns>The <see cref="ISignalConnection"/> representing the connection.</returns>
     ISignalConnection Connect(ISignalFlow flow);
 }
