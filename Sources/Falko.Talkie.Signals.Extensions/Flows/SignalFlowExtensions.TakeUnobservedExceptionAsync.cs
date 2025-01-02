a6428d4004e72356ed638ef883b8d4c7c0651847
Rima Falko <fembina@pm.me>
2025-01-02 06:13:59 +0100

    Revert "refactor take-exception method"
    This reverts commit 0cb600f5ebdb6845eb16429d96b8ebb768fef56d.

--- Sources/Falko.Talkie.Signals.Extensions/Flows/SignalFlowExtensions.TakeUnobservedExceptionAsync.cs
+++ Sources/Falko.Talkie.Signals.Extensions/Flows/SignalFlowExtensions.TakeUnobservedExceptionAsync.cs
@@ -13,8 +13,7 @@ public static partial class SignalFlowExtensions
         CancellationToken cancellationToken = default
     )
     {
-        return flow
-            .TakeAsync(signals => signals
+        return flow.TakeAsync(signals => signals
             .Where(signal => signal is IWithUnobservedExceptionSignal), cancellationToken)
             .InterceptOnSuccess(signal => Unsafe.As<IWithUnobservedExceptionSignal>(signal).Exception);
     }
