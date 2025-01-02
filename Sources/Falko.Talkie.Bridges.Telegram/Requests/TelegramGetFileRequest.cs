cb2373cf3a4b62644eb3aec016112686f25ebf28
Rima Falko <fembina@pm.me>
2025-01-02 02:05:15 +0100

    add telegram-request interface

--- Sources/Falko.Talkie.Bridges.Telegram/Requests/TelegramGetFileRequest.cs
+++ Sources/Falko.Talkie.Bridges.Telegram/Requests/TelegramGetFileRequest.cs
@@ -1,6 +1,8 @@
+using Talkie.Bridges.Telegram.Models;
+
 namespace Talkie.Bridges.Telegram.Requests;
 
-public sealed class TelegramGetFileRequest(string fileId)
+public sealed class TelegramGetFileRequest(string fileId) : ITelegramRequest<TelegramFile>
 {
     public string FileId => fileId;
 }
