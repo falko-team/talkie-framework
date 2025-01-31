a67fcaea00d3333fe8cfb470d5643bb04862f43d
Rima Falko <fembina@pm.me>
2025-01-31 21:54:59 +0100

    add link text style builders

---
+++ Sources/Falko.Talkie.Platforms/Models/Messages/Contents/Styles/LinkTextStyle.cs
@@ -0,0 +1,11 @@
+namespace Talkie.Models.Messages.Contents.Styles;
+
+public sealed record LinkTextStyle(int Offset, int Length, string Link) : IMessageTextStyle
+{
+    public IMessageTextStyle MutateTextRange(Func<MessageTextRange, MessageTextRange> rangeMutator)
+    {
+        return FromLink(Link).FromTextRange(rangeMutator(this.ToTextRange()));
+    }
+
+    public static LinkTextStyleBuilder FromLink(string link) => new(link);
+}
