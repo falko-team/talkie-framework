363c014eb4c8503e233b6ff129e8e9a5a7d3acb5
Rima Falko <fembina@pm.me>
2025-02-13 23:28:20 +0100

    remove ok property from telegram-response

--- Sources/Falko.Talkie.Bridges.Telegram/Responses/TelegramResponse.cs
+++ Sources/Falko.Talkie.Bridges.Telegram/Responses/TelegramResponse.cs
@@ -4,14 +4,11 @@ namespace Talkie.Bridges.Telegram.Responses;
 
 public sealed class TelegramResponse
 (
-    bool ok,
     int? errorCode = null,
     string? description = null,
     IReadOnlyDictionary<string, TextOrNumberValue>? parameters = null
 )
 {
-    public bool Ok => ok;
-
     public int? ErrorCode => errorCode;
 
     public string? Description => description;
