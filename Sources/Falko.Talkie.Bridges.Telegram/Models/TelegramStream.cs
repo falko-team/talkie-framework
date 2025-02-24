0a79bb06ded60d06b86f1c1f110c40f3655760ea
Rima Falko <fembina@pm.me>
2025-02-24 22:40:03 +0100

    optimized retries and add repeatable for telegram-stream request for telegram-client in telegram-bridge

--- Sources/Falko.Talkie.Bridges.Telegram/Models/TelegramStream.cs
+++ Sources/Falko.Talkie.Bridges.Telegram/Models/TelegramStream.cs
@@ -1,3 +1,6 @@
 namespace Talkie.Bridges.Telegram.Models;
 
-public readonly record struct TelegramStream(string Name, Stream Stream);
+public readonly record struct TelegramStream(int Identifier, Stream Stream, string? Name = null)
+{
+    public string ToAttach() => $"attach://{Identifier}";
+}
