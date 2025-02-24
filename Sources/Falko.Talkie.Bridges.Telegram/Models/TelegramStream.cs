7ce87f5e63c13399210a11bf25db83eeb54c69da
Rima Falko <rima@falko.email>
2025-02-24 22:42:01 +0100

    Add to-string ignore for stream property in telegram-stream

--- Sources/Falko.Talkie.Bridges.Telegram/Models/TelegramStream.cs
+++ Sources/Falko.Talkie.Bridges.Telegram/Models/TelegramStream.cs
@@ -1,6 +1,8 @@
+using System.Runtime.Serialization;
+
 namespace Talkie.Bridges.Telegram.Models;
 
-public readonly record struct TelegramStream(int Identifier, Stream Stream, string? Name = null)
+public readonly record struct TelegramStream(int Identifier, [property: IgnoreDataMember] Stream Stream, string? Name = null)
 {
     public string ToAttach() => $"attach://{Identifier}";
 }
