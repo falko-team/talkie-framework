6b3b64e5094be80bf4be00e77abc6044707adfda
Rima Falko <fembina@pm.me>
2025-02-28 18:16:23 +0100

    add support if IMessageStickerAttachmentController for telegram platform

--- Sources/Falko.Talkie.Platforms.Telegram/Controllers/AttachmentControllers/TelegramAttachmentController.cs
+++
@@ -1,6 +0,0 @@
-namespace Talkie.Controllers.AttachmentControllers;
-
-public sealed class TelegramAttachmentController : IAttachmentController
-{
-    public IImageAttachmentController ImageAttachment { get; } = new TelegramImageAttachmentController();
-}
