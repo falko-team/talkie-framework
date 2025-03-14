50bb8703409d9049e07c1f6325c5e2f7e6415081
Rima Falko <fembina@pm.me>
2025-03-14 15:15:11 +0100

    add more source-documentation

--- Sources/Falko.Talkie.Core/Concurrent/IParallelEnumerable.cs
+++ Sources/Falko.Talkie.Core/Concurrent/IParallelEnumerable.cs
@@ -1,6 +1,14 @@
 namespace Talkie.Concurrent;
 
+/// <summary>
+/// Represents a parallel enumerable. This enumerable can be enumerated in parallel.
+/// </summary>
+/// <typeparam name="T">The type of the elements in the collection.</typeparam>
 public interface IParallelEnumerable<T>
 {
+    /// <summary>
+    /// Creates a parallel enumerator for the collection.
+    /// </summary>
+    /// <returns>A parallel enumerator that can be used to enumerate the collection in parallel.</returns>
     IParallelEnumerator<T> GetParallelEnumerator();
 }
--- Sources/Falko.Talkie.Core/Concurrent/IParallelEnumerator.cs
+++ Sources/Falko.Talkie.Core/Concurrent/IParallelEnumerator.cs
@@ -2,7 +2,22 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace Talkie.Concurrent;
 
+/// <summary>
+/// Represents a parallel enumerator. This enumerator can be enumerated in parallel.
+/// </summary>
+/// <typeparam name="T">The type of the elements in the collection.</typeparam>
 public interface IParallelEnumerator<T>
 {
+    /// <summary>
+    /// Try to move the enumerator to the next element in the collection.
+    /// </summary>
+    /// <param name="item">
+    /// The next element in the collection if returned <see langword="true"/>;
+    /// or the default value of <typeparamref name="T"/> if returned <see langword="false"/>.
+    /// </param>
+    /// <returns>
+    /// <see langword="true"/> if the enumerator was successfully moved to the next element;
+    /// <see langword="false"/> if the enumerator has passed the end of the collection.
+    /// </returns>
     bool TryMoveNext([MaybeNullWhen(false)] out T item);
 }
