c9aa088e47a04b0f80024113a1a719fc8072b486
Rima Falko <rima@falko.email>
2025-03-14 15:15:11 +0100

    add more source-documentation

--- Sources/Falko.Talkie.Core/Concurrent/TaskExtensions.AsValueTask.cs
+++ Sources/Falko.Talkie.Core/Concurrent/TaskExtensions.AsValueTask.cs
@@ -2,5 +2,10 @@ namespace Talkie.Concurrent;
 
 public static partial class TaskExtensions
 {
+    /// <summary>
+    /// Converts the specified <see cref="Task"/> to a <see cref="ValueTask"/>.
+    /// </summary>
+    /// <param name="task">The task to convert.</param>
+    /// <returns>The converted <see cref="ValueTask"/>.</returns>
     public static ValueTask AsValueTask(this Task task) => new(task);
 }
